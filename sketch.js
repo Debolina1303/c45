c77a468a532e7108c79c6d853eae1a2122b32b6d
Debolina1303 <79790212+Debolina1303@users.noreply.github.com>
2022-06-24 08:09:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,149 @@
+var score =0;
+var gun,meteors, bullet, backBoard;
+
+var gunImg,bubbleImg, bulletImg, blastImg;
+
+var meteorGroup,  bulletGroup;
+
+
+var life =3;
+var score=0;
+var gameState=1
+
+function preload(){
+  gunImg = loadImage("gun1.png")
+  blastImg = loadImage("blast.png")
+  laserImg = loadImage("laser.jpg")
+  meteorsImg = loadImage("meteors.jpg")
+
+}
+function setup() {
+  createCanvas(800, 800);
+
+  
+  
+  gun= createSprite(100, height/2, 50,50);
+  gun.addImage(gunImg)
+  gun.scale=0.2
+  
+  bulletGroup = createGroup();   
+  meteorGroup = createGroup();   
+   
+  
+  heading= createElement("h1");
+  scoreboard= createElement("h1");
+}
+
+function draw() {
+  background("#BDA297");
+  
+  heading.html("Life: "+life)
+  heading.style('color:red'); 
+  heading.position(150,20)
+
+  scoreboard.html("Score: "+score)
+  scoreboard.style('color:red'); 
+  scoreboard.position(width-200,20)
+
+  if(gameState===1){
+    gun.y=mouseY  
+
+    
+
+    if (frameCount % 100 === 0) {
+      meteordrop();
+    }
+
+    if(keyDown("space")){
+      shootBullet();
+    }
+
+   
+    
+    if (meteorGroup.collide(ground)) {
+      handleGameover(meteorGroup);
+    }
+    
+    /*if(blueBubbleGroup.(bulletGroup)){
+      handleBubbleCollision(blueBubbleGroup);
+    }*/
+
+    /*if(blueBubbleGroup.collide(bulletGroup)){
+      handleBubbleCollision();
+    }*/
+    
+    /*if(blueBubbleGroup.collide()){
+      handleBubbleCollision(blueBubbleGroup);
+    }*/
+    
+    
+
+    drawSprites();
+  }
+    
+  
+}
+
+
+function meteordrop(){
+  meteor = createSprite(800,random(20,780),40,40);
+  meteor.addImage(redBubbleImg);
+  meteor.scale = 0.1;
+  meteor.velocityX = -8;
+  meteor.lifetime = 400;
+  meteorGroup.add(meteor);
+}
+
+function shootBullet(){
+  bullet= createSprite(150, width/2, 50,20)
+  bullet.y= gun.y-20
+  bullet.addImage(bulletImg)
+  bullet.scale=0.12
+  bullet.velocityX= 7
+  bulletGroup.add(bullet)
+}
+
+function handleBubbleCollision(meteorGroup){
+    if (life > 0) {
+       score=score+1;
+    }
+
+     blast= createSprite(bullet.x+60, bullet.y, 50,50);
+    blast.addImage(blastImg) 
+
+    /* blast= sprite(bullet.x+60, bullet.y, 50,50);
+    blast.addImage(blastImg) */
+
+    /* blast= createSprite(bullet.x+60, bullet.y, 50,50);
+    blast.add(blastImg) */
+
+    /* blast= createSprite(bullet.x+60, bullet.y, 50,50);
+    image(blastImg) */
+    
+    blast.scale=0.3
+    blast.life=20
+    bulletGroup.destroyEach()
+    meteorGroup.destroyEach()
+}
+
+function handleGameover(meteorGroup){
+  
+    life=life-1;
+    bubbleGroup.destroyEach();
+    
+
+    if (life === 0) {
+      gameState=2
+      
+      swal({
+        title: `Game Over`,
+        text: "Oops you lost the game....!!!",
+        text: "Your Score is " + score,
+        imageUrl:
+          "https://cdn.shopify.com/s/files/1/1061/1924/products/Thumbs_Down_Sign_Emoji_Icon_ios10_grande.png",
+        imageSize: "100x100",
+        confirmButtonText: "Thanks For Playing"
+      });
+    }
+  
+}
